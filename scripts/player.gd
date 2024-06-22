d69ac60610e3cbc146f2d9698fa3deb0d57c85a6
Carson Ball <carsonjball@gmail.com>
2024-06-22 17:53:00 -0400

    forward to down and up to down working

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody3D
 
 const SPEED = 8.0
 const JUMP_VELOCITY = 12
+# the smaller this value, the faster it goes
+const CAMERA_ROTATION_SPEED = 50
 
 # Get the gravity from the project settings to be synced with RigidBody nodes. (nope!)
 var gravity = 24.0
@@ -10,19 +12,96 @@ var sensitivity = 0.002
 var current_pull = 1
 var positive = false
 
+var x_to_set = 0.0
+var y_to_set = 0.0
+var z_to_set = 0.0
+var x_updates = 0
+var y_updates = 0
+var z_updates = 0
+
+
+
 @onready var camera_3d = $Camera3D
 @onready var ray_cast_3d = $Camera3D/RayCast3D
 @onready var gridmap = preload("res://scripts/GridMap.gd").new()
+@onready var collision_shape_3d = $CollisionShape3D
+@onready var body = $Body
+@onready var stick = $stick
 
+
+func set_top_level(t):
+	camera_3d.top_level = t
+	collision_shape_3d.top_level = t
+	body.top_level = t
+	stick.top_level = t
+
+func is_rotating():
+	if x_updates <= 0 and y_updates <= 0 and z_updates <= 0:
+		return false
+	else:
+		return true
+
+func update_rotation():
+	if y_updates > 0:
+		rotate_y(y_to_set)
+		y_updates -= 1
+		
+	if x_updates > 0:
+		rotate_x(x_to_set)
+		x_updates -= 1
+		
+	if z_updates > 0:
+		rotate_z(z_to_set)
+		z_updates -= 1
+	
 
 
 func change_rotation(x, y ,z):
-	rotation.x = 0
-	rotation.y = 0
-	rotation.z = 0
-	rotate_x(deg_to_rad(x))
-	rotate_y(deg_to_rad(y))
-	rotate_z(deg_to_rad(z))
+	# get difference
+	#set_top_level(true)
+	#transform.basis = Basis()
+	#set_top_level(false)
+	
+
+	if x == null:
+		pass
+	else:
+		if x == 0:
+			if rotation.z < 0:
+				x_to_set = rotation.z / CAMERA_ROTATION_SPEED
+			else: 
+				x_to_set = -rotation.z / CAMERA_ROTATION_SPEED
+		else:
+			x_to_set = (deg_to_rad(abs(x)) - rotation.x) * (x / abs(x)) / CAMERA_ROTATION_SPEED
+		x_updates = CAMERA_ROTATION_SPEED
+	
+	if y == null:
+		pass
+	else:
+		if y == 0:
+			if rotation.y < 0:
+				y_to_set = rotation.y / CAMERA_ROTATION_SPEED
+			else:
+				y_to_set = -rotation.y / CAMERA_ROTATION_SPEED
+		else:
+			y_to_set = (deg_to_rad(abs(y)) - rotation.y) * (y / abs(y)) / CAMERA_ROTATION_SPEED
+		y_updates = CAMERA_ROTATION_SPEED
+	
+	if z == null:
+		pass
+	else:
+		if z == 0:
+			if rotation.z < 0:
+				z_to_set = rotation.z / CAMERA_ROTATION_SPEED
+			else:
+				z_to_set = -rotation.z / CAMERA_ROTATION_SPEED
+		else:
+			z_to_set = (deg_to_rad(abs(z)) - rotation.z) * (z / abs(z)) / CAMERA_ROTATION_SPEED
+		z_updates = CAMERA_ROTATION_SPEED
+		
+		
+	
+	print("X: "+str(x_to_set)+" Y: "+str(y_to_set)+" Z: "+str(z_to_set))
 	
 
 
@@ -43,37 +122,37 @@ func change_gravity(raycast_object):
 		print("left")
 		current_pull = 0
 		positive = false
-		change_rotation(0, 0, -90)
+		change_rotation(null, null, -90)
 		
 	elif block.x - face.x > 0 and (current_pull != 0 or positive != true):
 		print("right")
 		current_pull = 0
 		positive = true
-		change_rotation(0, 0, 90)
+		change_rotation(null, 0, 90)
 		
 	elif block.y - face.y < 0 and (current_pull != 1 or positive != false):
 		print("down")
 		current_pull = 1
 		positive = false
-		change_rotation(0, 0, 0)
+		change_rotation(0, null, null)
 		
 	elif block.y - face.y > 0 and (current_pull != 1 or positive != true):
 		print("up")
 		current_pull = 1
 		positive = true
-		change_rotation(180, 0, 0)
+		change_rotation(180, null, null)
 		
 	elif block.z - face.z < 0 and (current_pull != 2 or positive != false):
 		print("backward")
 		current_pull = 2
 		positive = false
-		change_rotation(90, 0, 0)
+		change_rotation(90, null, null)
 		
 	elif block.z - face.z > 0 and (current_pull != 2 or positive != true):
 		print("forward")
 		current_pull = 2
 		positive = true
-		change_rotation(-90, 0, 0)
+		change_rotation(-90, null, null)
 		
 
 
@@ -81,10 +160,12 @@ func change_gravity(raycast_object):
 func _ready(): # setup
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
+	set_top_level(false)
+	
 	
 func _input(event):
 	# Mouse input
-	if event is InputEventMouseMotion:
+	if event is InputEventMouseMotion and is_rotating() == false:
 		if positive:
 			if current_pull == 0:
 				rotate_x(event.relative.x * sensitivity)
@@ -198,6 +279,8 @@ func _physics_process(delta):
 				change_gravity(ray_cast_3d)
 				#ray_cast_3d.get_collider().place_block(ray_cast_3d.get_collision_point() + 
 				#ray_cast_3d.get_collision_normal(), 0)
+				
+	update_rotation()
 	
 	transform = transform.orthonormalized()
 	camera_3d.transform = camera_3d.transform.orthonormalized()
