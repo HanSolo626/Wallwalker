25e8f6aa97790cb51f2c3c760d516d221c9394f0
HanSolo626 <carsonjball@gmail.com>
2024-11-22 20:53:57 -0500

    added area switch fixed some bugs

--- scripts/test_world.gd
+++ scripts/test_world.gd
@@ -2,12 +2,11 @@ extends Node3D
 
 @onready var platform = $Platform
 @onready var user_interface = $Control/UserInterface
-@onready var stumper_door = $StumperDoor
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	platform.glide_in_loop([Vector3(0, 5, 0), Vector3(-8, 5, 0), Vector3(-8, 5, -8), Vector3(0, 5, -8)], 0.1, 0.5)
-	stumper_door.open_door()
+	
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,3 +16,7 @@ func _process(delta):
 
 func _on_player_player_killed():
 	user_interface.enable_death_screen()
+
+
+func _on_area_switch_body_entered(body):
+	print("worked")
