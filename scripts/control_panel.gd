0ccb777693162c2f7358e442a44f069e59fecf19
HanSolo626 <carsonjball@gmail.com>
2024-11-23 21:23:45 -0500

    can now bind to switches

--- scripts/control_panel.gd
+++ scripts/control_panel.gd
@@ -1,6 +1,9 @@
 extends StaticBody3D
 
 
+var player_present = false
+var being_lashed = false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,3 +12,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_player_detection_body_entered(body):
+	player_present = true
+
+
+func _on_player_detection_body_exited(body):
+	player_present = false
