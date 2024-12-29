473b1947446e51d7ece6c7e36d1dbef93fc9c4f5
HanSolo626 <carsonjball@gmail.com>
2024-12-28 21:11:40 -0500

    created victory menu

--- alpha_levels/lashing_binding_test_area/lashing_binding_test_area.gd
+++ alpha_levels/lashing_binding_test_area/lashing_binding_test_area.gd
@@ -5,8 +5,9 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	platform_2.glide_in_loop([Vector3(-6.362, 8.705, -15.221), Vector3(-6.362, 8.705, 13.208)], 0.1, 0)
-
+	platform_2.glide_in_loop(
+		[Vector3(-6.362, 8.705, -15.221), Vector3(-6.362, 8.705, 13.208)], 0.1, 0
+		)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
