0a98407bba31a53aeef29e669dda641bff13f4f7
HanSolo626 <carsonjball@gmail.com>
2025-01-03 17:11:58 -0500

    fix merge

--- alpha_levels/lashing_binding_test_area/lashing_binding_test_area.gd
+++ alpha_levels/lashing_binding_test_area/lashing_binding_test_area.gd
@@ -3,9 +3,15 @@ extends Node3D
 @onready var platform_2 = $Platform2
 @onready var control = $Control
 @onready var floor_switch = $FloorSwitch
+@onready var wooden_lever = $WoodenLever
 
-
-
+func load_and_play_track():
+	var track = preload("res://resources/tracks/Protoss2.mp3")
+	var player = AudioStreamPlayer.new()
+	player.stream = track
+	player.autoplay = true
+	add_child(player)
+	
 func end_game():
 	control.get_victory_menu().process_mode = PROCESS_MODE_ALWAYS
 	#control.get_pause_menu().set_mouse_filter(2)
@@ -14,23 +20,11 @@ func end_game():
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	load_and_play_track()
 	platform_2.glide_in_loop(
 		[Vector3(-6.362, 8.705, -15.221), Vector3(-6.362, 8.705, 13.208)], 0.1, 0
 		)
-@onready var wooden_lever = $WoodenLever
 
-
-func load_and_play_track():
-	var track = preload("res://resources/tracks/Protoss2.mp3")
-	var player = AudioStreamPlayer.new()
-	player.stream = track
-	player.autoplay = true
-	add_child(player)
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	load_and_play_track()
-	platform_2.glide_in_loop([Vector3(-6.362, 8.705, -15.221), Vector3(-6.362, 8.705, 13.208)], 0.1, 0)
 
 
 
