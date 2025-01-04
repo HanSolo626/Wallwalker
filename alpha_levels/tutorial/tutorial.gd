f9333725857d8ce2edd37a6b908cbc772bc1166b
HanSolo626 <carsonjball@gmail.com>
2025-01-03 21:19:55 -0500

    built tutorial level

--- alpha_levels/tutorial/tutorial.gd
+++ alpha_levels/tutorial/tutorial.gd
@@ -2,7 +2,17 @@ extends Node3D
 
 
 @onready var control = $Control
+@onready var game_end_floor_switch = $GameEndFloorSwitch
+
+@onready var door = $Door
+@onready var door_2 = $Door2
+@onready var door_3 = $Door3
+
+@onready var first_lever = $FirstLever
+@onready var second_lever = $SecondLever
+
 @onready var floor_switch = $FloorSwitch
+
 
 func load_and_play_track():
 	var track = preload("res://resources/tracks/Protoss2.mp3")
@@ -26,8 +36,9 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if floor_switch.glowing:
+	if game_end_floor_switch.glowing:
 		end_game()
+		
 
 
 func _on_player_player_killed():
@@ -39,4 +50,18 @@ func _on_control_death_animation_done():
 
 
 func _on_player_action_key_pressed():
-	pass
+	if first_lever.lever_usable():
+		first_lever.set_lever_on()
+		first_lever.set_disabled()
+		door.glide_up(5, 0.05)
+	if second_lever.lever_usable():
+		second_lever.set_lever_on()
+		second_lever.set_disabled()
+		door_2.glide_up(5, 0.05)
+
+
+func _on_floor_switch_activated():
+	door_3.glide_to_position(Vector3(-69.388, 7.903, -11.875), 0.05)
+
+func _on_floor_switch_deactivated():
+	door_3.glide_to_position(Vector3(-69.388, 2.903, -11.875), 0.05)
