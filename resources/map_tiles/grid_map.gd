2bf07abad9dfe2573544f197ce69588c33a37d00
HanSolo626 <carsonjball@gmail.com>
2025-01-09 18:10:16 -0500

    added torch to GridMap

--- resources/map_tiles/grid_map.gd
+++ resources/map_tiles/grid_map.gd
@@ -1,5 +1,18 @@
 extends GridMap
 
+const TORCH = preload("res://resources/map_tiles/torch/torch.tscn")
 
 func is_grid_map():
 	pass
+	
+func _ready():
+	var torches = get_used_cells_by_item(0)
+	for torch in torches:
+		var pos = to_global(map_to_local(torch))
+		var torch_y_rotation = deg_to_rad(90 * get_cell_item_orientation(torch))
+		var new_torch = TORCH.instantiate()
+		new_torch.rotate_y(torch_y_rotation)
+		new_torch.transform.origin = pos
+		add_child(new_torch)
+	for torch in torches:
+		set_cell_item(torch, -1)
