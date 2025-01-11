d7a2b0456ab30b61ced3ed88376c3980c61baa04
HanSolo626 <carsonjball@gmail.com>
2025-01-11 08:53:41 -0500

    fixed torch orientation and lashing collision

--- resources/map_tiles/grid_map.gd
+++ resources/map_tiles/grid_map.gd
@@ -9,9 +9,16 @@ func _ready():
 	var torches = get_used_cells_by_item(0)
 	for torch in torches:
 		var pos = to_global(map_to_local(torch))
-		var torch_y_rotation = deg_to_rad(90 * get_cell_item_orientation(torch))
+		var torch_y_rotation = get_cell_item_orientation(torch)
+		if torch_y_rotation == 22:
+			torch_y_rotation = 270
+		elif torch_y_rotation == 16:
+			torch_y_rotation = 90
+		elif torch_y_rotation == 10:
+			torch_y_rotation = 180
+		
 		var new_torch = TORCH.instantiate()
-		new_torch.rotate_y(torch_y_rotation)
+		new_torch.rotate_y(deg_to_rad(torch_y_rotation))
 		new_torch.transform.origin = pos
 		add_child(new_torch)
 	for torch in torches:
