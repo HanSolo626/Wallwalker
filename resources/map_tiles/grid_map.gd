3b39bacf5980608f8e38256aded5bcd7791d607d
HanSolo626 <carsonjball@gmail.com>
2025-04-02 17:47:00 -0400

    fully integrated windows into GridMap

--- resources/map_tiles/grid_map.gd
+++ resources/map_tiles/grid_map.gd
@@ -1,11 +1,13 @@
 extends GridMap
 
 const TORCH = preload("res://resources/map_tiles/torch/torch.tscn")
+const WINDOW = preload("res://resources/map_tiles/window/window.tscn")
 
 func is_grid_map():
 	pass
 	
 func _ready():
+	# Add torches
 	var torches = get_used_cells_by_item(0)
 	for torch in torches:
 		var pos = to_global(map_to_local(torch))
@@ -23,3 +25,25 @@ func _ready():
 		add_child(new_torch)
 	for torch in torches:
 		set_cell_item(torch, -1)
+		
+	# Add windows
+	# get window manager
+	var manager = $"../WindowManager"
+	var windows = get_used_cells_by_item(1)
+	for window in windows:
+		var pos = to_global(map_to_local(window))
+		var window_y_rotation = get_cell_item_orientation(window)
+		print(window_y_rotation)
+		if window_y_rotation == 22:
+			window_y_rotation = 270
+		elif window_y_rotation == 16:
+			window_y_rotation = 90
+		elif window_y_rotation == 10:
+			window_y_rotation = 180
+		
+		var new_window = WINDOW.instantiate()
+		new_window.rotate_y(deg_to_rad(window_y_rotation))
+		new_window.transform.origin = pos
+		manager.add_child(new_window)
+	for window in windows:
+		set_cell_item(window, -1)
